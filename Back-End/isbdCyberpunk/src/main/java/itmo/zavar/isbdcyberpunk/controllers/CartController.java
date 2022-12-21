9838e040a1a0ea1b886c876162d341951b56f0f3
Zavar30 <yarusking@mail.ru>
2022-12-22 00:03:48 +0300

    backend: update confirmOrder method

--- Back-End/isbdCyberpunk/src/main/java/itmo/zavar/isbdcyberpunk/controllers/CartController.java
+++ Back-End/isbdCyberpunk/src/main/java/itmo/zavar/isbdcyberpunk/controllers/CartController.java
@@ -5,8 +5,10 @@ import itmo.zavar.isbdcyberpunk.models.shop.order.OrderEntity;
 import itmo.zavar.isbdcyberpunk.models.shop.order.OrderStatusEntity;
 import itmo.zavar.isbdcyberpunk.models.shop.order.OrderStructureEntity;
 import itmo.zavar.isbdcyberpunk.models.shop.storage.StorageElementEntity;
+import itmo.zavar.isbdcyberpunk.models.user.info.BillingEntity;
 import itmo.zavar.isbdcyberpunk.models.user.list.ListCustomersEntity;
 import itmo.zavar.isbdcyberpunk.payload.request.RemoveFromCartRequest;
+import itmo.zavar.isbdcyberpunk.payload.response.MessageResponse;
 import itmo.zavar.isbdcyberpunk.repository.*;
 import itmo.zavar.isbdcyberpunk.security.services.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,6 +48,12 @@ public class CartController {
     @Autowired
     private ListCustomersEntityRepository customersEntityRepository;
 
+    @Autowired
+    private StorageElementEntityRepository storageElementEntityRepository;
+
+    @Autowired
+    private BillingEntityRepository billingEntityRepository;
+
     @PostMapping("/removeFromCart")
     @PreAuthorize("isAuthenticated()")
     @Transactional
@@ -59,29 +68,51 @@ public class CartController {
     @Transactional
     public ResponseEntity<?> confirmOrder() {
         UserDetailsImpl principal = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Optional<ListCustomersEntity> listById = customersEntityRepository.findByUserId_Id(principal.id());
-        List<ShoppingCartEntity> list = cartEntityRepository.findAllByCustomerId_Id(listById.get().getId());
+        Optional<ListCustomersEntity> customerById = customersEntityRepository.findByUserId_Id(principal.id());
+        if(customerById.isEmpty())
+            return ResponseEntity.status(400).body(new MessageResponse("Пользователь не найден"));
+        List<ShoppingCartEntity> list = cartEntityRepository.findAllByCustomerId_Id(customerById.get().getId());
         Optional<ListCustomersEntity> customer = customersEntityRepository.findByUserId_Id(principal.id());
         List<StorageElementEntity> ids = new ArrayList<>();
         Long sum = 0L;
 
-        if(list.isEmpty())
-            return ResponseEntity.status(400).build();
+        if(list.isEmpty()) {
+            return ResponseEntity.status(400).body(new MessageResponse("Корзина пуста"));
+        }
 
         for(ShoppingCartEntity shoppingCartEntity : list)  {
             ids.add(shoppingCartEntity.getStorageElementEntity());
             sum += shoppingCartEntity.getStorageElementEntity().getPrice();
         }
-        Optional<OrderStatusEntity> byId = statusEntityRepository.findById(1L);//get "оформлен"
+        Optional<OrderStatusEntity> byId = statusEntityRepository.findById(2L);//get "выполнен"
         if(byId.isPresent() && customer.isPresent()) {
             OrderEntity orderEntity = new OrderEntity(Date.from(Instant.now()), sum, byId.get(), customer.get());
             OrderEntity save = orderEntityRepository.save(orderEntity);
-            ids.forEach(entity -> orderStructureEntityRepository.save(new OrderStructureEntity(entity, save)));
+            for (StorageElementEntity entity : ids) {
+                if (entity.getCount() > 0) {
+                    entity.setCount(entity.getCount() - 1);
+                    StorageElementEntity save1 = storageElementEntityRepository.save(entity);
+                    orderStructureEntityRepository.save(new OrderStructureEntity(save1, save));
+                } else {
+                    TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+                    return ResponseEntity.badRequest().body(new MessageResponse("На складе недостаточно '" + entity.getCyberwareId().getName() + "'"));
+                }
+            }
         } else {
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
             return ResponseEntity.status(400).build();
         }
+        BillingEntity billing = customerById.get().getBillingId();
 
-        cartEntityRepository.deleteAllByCustomerId_Id(listById.get().getId());
+        if(billing.getSum() < sum) {
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+            return ResponseEntity.status(412).body(new MessageResponse("Недостаточно средств"));
+        }
+
+        billing.setSum(billing.getSum() - sum);
+        cartEntityRepository.deleteAllByCustomerId_Id(customerById.get().getId());
+
+        billingEntityRepository.save(billing);
 
         return ResponseEntity.ok().build();
     }
